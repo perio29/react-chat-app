e7075a51211a1e683bf0f5dcf8a005c12c37a488
perio29 <tokuzou10yohji@gmail.com>
2021-09-25 09:40:13 +0900

    router-edit#1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,6 @@
-import styled from "styled-components";
-import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
-import { HomePage } from "./components/HomePage";
-import { LoginPage } from "./components/LoginPage";
-import { RoomPage } from "./components/RoomPage";
-
+import Router from "./Route/Router";
 const App = () => {
-  return (
-    <BrowserRouter>
-      <SDiv>
-        <Link to="/">Home</Link>
-        <br />
-        <br />
-        <Link to="/login">Login</Link>
-        <br />
-        <br />
-        <Link to="/room">Room</Link>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route exact path="/room/:roomId">
-            <RoomPage />
-          </Route>
-        </Switch>
-      </SDiv>
-    </BrowserRouter>
-  );
+  return <Router />;
 };
 
 export default App;
-
-const SDiv = styled.div`
-  margin-top: 20px;
-  text-align: center;
-`;
