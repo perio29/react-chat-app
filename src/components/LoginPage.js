1899fa95d20bbe207be4934fa91a494d66d29aa4
perio29 <tokuzou10yohji@gmail.com>
2021-09-25 16:50:06 +0900

    firebase authenticationでログイン機能 #4

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import { auth }  from "../firebase/firebase";
+
 export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = e.target.elements;
+    auth.createUserWithEmailAndPassword(email.value, password.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div>
       <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
     </div>
   );
 };
